590c5a76949b0f1ce7ecf16bbd5fd74c7f241b6e
Chauni <chaunidbrown@gmail.com>
2023-04-28 09:57:16 -0400

    added functionality to the temperature converters

--- scripts/index2.js
+++ scripts/index2.js
@@ -3,26 +3,20 @@
 // gather inputs
 
 const convertBtnEl = document.getElementById('convertBtn');
-convertBtnEl.onclick = onConvertBtnClicked
+convertBtnEl.onclick = onConvertBtnClicked;
 
 const farenheitTempEl = document.getElementById('farenheitTemp')
 const celsiusTempEl = document.getElementById('celsiusTemp');
 
 const convertOutputEl = document.getElementById('convertOutput');
 
-const resetBtnEl = document.getElementById('resetBtn');
-
 
 // perform calculations
 
 function onConvertBtnClicked() {
-    if (farenheitTempEl == farenheitTemp) {
+    if (celsiusTempEl == celsiusTemp) {
 
-        const output = (farenheitTempEl.value - 32) * 5 / 9;
+        const output = (celsiusTempEl.value * 9/ 5) + 32;
         convertOutputEl.value = output;
-}
-}
-
-function onResetBtnClicked() {
-    document.getElementById('farenheitTempEl').value = "  ";
+    }
 }
